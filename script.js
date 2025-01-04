99631cb0a7fd1990037c74e1276aa2a2c68cd250
prodsimu <139299982+prodsimu@users.noreply.github.com>
2025-01-04 20:09:18 -0300

    feat: implement a slider to change the size of the grid

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const gridside = 600;
-let rows = 16
-let cols = 16;
+const GRIDSIDE = 600;
+let squaresPerSide = 16;
 
 
 const sketchArea = document.querySelector("#sketch-area");
-sketchArea.style.width = `${gridside}px`
-sketchArea.style.height = `${gridside}px`
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+
+sliderValue.textContent = `${slider.value} x ${slider.value} Resolution`
+
+sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`
 
 function setBackgroundColor() {
   this.style.backgroundColor = "black";
 }
 
-function createGridCells() {
-  for (let i = 0; i < (rows * cols); i++) {
+function createGridCells(squaresPerSide) {
+  const numOfSquares = (squaresPerSide * squaresPerSide);
+  const widthAndHeight = `${(GRIDSIDE / squaresPerSide) - 2}px`;
+
+  for (let i = 0; i < numOfSquares; i++) {
     const gridCell = document.createElement("div");
 
-    gridCell.style.width = `${(gridside / cols) - 2}px`
-    gridCell.style.height = `${(gridside / rows) - 2}px`
+    gridCell.style.width = gridCell.style.height = widthAndHeight
     gridCell.classList.add("cell");
 
     sketchArea.appendChild(gridCell);
@@ -26,4 +32,17 @@ function createGridCells() {
 }
 
 
-createGridCells();
+function removeGrideCells() {
+  while (sketchArea.firstChild) {
+    sketchArea.removeChild(sketchArea.firstChild);
+  }
+
+}
+
+
+slider.oninput = function () {
+  let text = `${this.value} x ${this.value}`
+  sliderValue.innerHTML = text;
+  removeGrideCells();
+  createGridCells(this.value);
+}
